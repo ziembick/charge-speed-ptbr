f67ff478449185479305247f6186e94f8612d783
Paulo Ziembick <pauloziembick@gmail.com>
2022-12-08 21:25:15 -0300

    js file updated

--- js/script.js
+++ js/script.js
@@ -1,25 +1,46 @@
-let time = 1500,
-  currentImageIndex = 0,
-  images = document.querySelectorAll("#slider img");
-  max = images.length;
+let slide = document.getElementById("slide");
+let images = [
+  "img/body-pic__slide-1.jpg",
+  "img/body-pic__slide-2.jpg",
+  "img/body-pic__slide-3.jpg",
+];
+let i = 0;
+max = images.length
 
-function nextImage() {
 
+function slideShow () {
+  slide.src = images[i];
 
-  currentImageIndex++
+  if (i < images.length - 1) {
+    i++
+  } 
 
-  if(currentImageIndex == max) {
-    currentImageIndex = 0
-  }
-
-  images[currentImageIndex].classList.add('selected')
-
+  setTimeout(slideShow, 1000)
 }
 
-function start() {
-  setInterval(() => {
-    nextImage ()
-  }, time);
-}
+slideShow()
 
-window.addEventListener("load", start);
+// let time = 1500,
+//   currentImageIndex = 0,
+//   images = document.querySelectorAll("#slider img");
+//   max = images.length;
+
+// function nextImage() {
+
+//   currentImageIndex++
+
+//   if(currentImageIndex == max) {
+//     currentImageIndex = 0
+//   }
+
+//   images[currentImageIndex].classList.add('selected')
+
+// }
+
+// function start() {
+//   setInterval(() => {
+//     nextImage ()
+//   }, time);
+// }
+
+// window.addEventListener("load", start);
