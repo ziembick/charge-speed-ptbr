06350fd7b6759699dbf409114396f2355ae78203
Paulo Ziembick <pauloziembick@gmail.com>
2022-12-14 22:38:41 -0300

    modal pecas

--- js/script.js
+++ js/script.js
@@ -1,54 +1,51 @@
 //seleções de tabs
-const tab1 = document.querySelector('#tab1')
-const tab2 = document.querySelector('#tab2')
-const tab3 = document.querySelector('#tab3')
-const solo = document.querySelector('.section-container__categorias-solo')
-const solo2 = document.querySelector('.section-container__categorias-solo2')
-const solo3 = document.querySelector('.section-container__categorias-solo3')
+const tab1 = document.querySelector("#tab1");
+const tab2 = document.querySelector("#tab2");
+const tab3 = document.querySelector("#tab3");
+const solo = document.querySelector(".section-container__categorias-solo");
+const solo2 = document.querySelector(".section-container__categorias-solo2");
+const solo3 = document.querySelector(".section-container__categorias-solo3");
 
+tab1.addEventListener("click", function () {
+  solo.classList.add("selected");
+  solo2.classList.remove("selected");
+  solo3.classList.remove("selected");
+});
 
-tab1.addEventListener('click', function() {
-  solo.classList.add('selected')
-  solo2.classList.remove('selected')
-  solo3.classList.remove('selected')
-})
+tab2.addEventListener("click", function () {
+  solo2.classList.add("selected");
+  solo.classList.remove("selected");
+  solo3.classList.remove("selected");
+});
 
-tab2.addEventListener('click', function (){
-  solo2.classList.add('selected')
-  solo.classList.remove('selected')
-  solo3.classList.remove('selected')
-})
-
-tab3.addEventListener('click', function(){
-  solo3.classList.add('selected')
-  solo2.classList.remove('selected')
-  solo.classList.remove('selected')
-})
+tab3.addEventListener("click", function () {
+  solo3.classList.add("selected");
+  solo2.classList.remove("selected");
+  solo.classList.remove("selected");
+});
 
 //slides de imagens
 
 let slide = document.getElementById("slide");
 let images = [
-   "img/body-pic__slide-1.jpg",
-   "img/body-pic__slide-2.jpg",
-   "img/body-pic__slide-3.jpg",
+  "img/body-pic__slide-1.jpg",
+  "img/body-pic__slide-2.jpg",
+  "img/body-pic__slide-3.jpg",
 ];
- let i = 0;
- max = images.length
+let i = 0;
+max = images.length;
 
-
- function slideShow () {
-   slide.src = images[i];
+function slideShow() {
+  slide.src = images[i];
 
   if (i < images.length - 1) {
-    i++
-  }  
+    i++;
+  }
 
-   setTimeout(slideShow, 900)
- }
+  setTimeout(slideShow, 900);
+}
 
- slideShow()
-
+slideShow();
 
 //login-btn
 const createProductButton = document.querySelector("#create-product");
@@ -58,24 +55,32 @@ const openModal = () => dialog.showModal();
 
 createProductButton.addEventListener("click", openModal);
 
-cancelButton.addEventListener("click", () => dialog.close());
 const inputs = document.querySelectorAll("input");
 
 const saveButton = document.querySelector("#save");
 
-function logar () {
-    const login = document.getElementById('login').value;
-    const senha = document.getElementById('senha').value;
-    const form = document.querySelector('form');
-    form.reset();
+function logar() {
+  const login = document.getElementById("login").value;
+  const senha = document.getElementById("senha").value;
+  const form = document.querySelector("form");
+  form.reset();
 
-    if(login == 'admin' && senha == 'admin'){
-        alert('Login efetuado com sucesso!');
-        createProductButton.textContent = "Sair"
-    }else {
-        alert('Usuário ou senha inválidas')
-    }
+  if (login == "admin" && senha == "admin") {
+    alert("Login efetuado com sucesso!");
+    createProductButton.textContent = "Sair";
+  } else {
+    alert("Usuário ou senha inválidas");
+  }
 }
+
+//peças modal
+const createPecasAero = document.querySelector('#pecas-aerodinamicas')
+const dialog2 = document.querySelector('.modal-pecas__dialog')
+
+const abrirModal = () => dialog2.showModal()
+
+createPecasAero.addEventListener('click', abrirModal)
+
 
 
 
