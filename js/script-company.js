e23da989b44b151f86b3e90b05f7f5d7e70a4d13
Paulo Ziembick <pauloziembick@gmail.com>
2022-12-18 20:42:14 -0300

    sobre recrutar js

--- js/script-company.js
+++ js/script-company.js
@@ -2,13 +2,17 @@ const sobre = document.querySelector("#sobre");
 const recrutar = document.querySelector("#recrutar");
 const sobreTab = document.querySelector(".tabMenu-about__table");
 const recrutarTab = document.querySelector(".tabMenu-recrutar__table");
+const recrutarTexto = document.querySelector(".recrutar-text-p")
 
 sobre.addEventListener("click", function () {
   sobreTab.classList.add("selected");
   recrutarTab.classList.remove("selected");
+  recrutarTexto.classList.add('hide')
 });
 
 recrutar.addEventListener("click", function () {
   recrutarTab.classList.add("selected");
   sobreTab.classList.remove("selected");
+  recrutarTexto.classList.remove('hide')
+
 });
