2c85177d8d42c756b9fa602d19c2ec13e611c72c
Paulo Ziembick <pauloziembick@gmail.com>
2022-12-19 22:30:22 -0300

    login btn company js

--- js/script-company.js
+++ js/script-company.js
@@ -37,6 +37,31 @@ function ocultar() {
 
 ocultar();
 
+// tela de login
+const createProductButton = document.querySelector("#create-product");
+const dialog = document.querySelector(".create-product_dialog");
+
+const openModal = () => dialog.showModal();
+
+createProductButton.addEventListener("click", openModal);
+
+const inputs = document.querySelectorAll("input");
+
+const saveButton = document.querySelector("#save");
+
+function logar() {
+  const login = document.getElementById("login").value;
+  const senha = document.getElementById("senha").value;
+  const form = document.querySelector("form");
+  form.reset();
+
+  if (login == "admin" && senha == "admin") {
+    alert("Login efetuado com sucesso!");
+    createProductButton.textContent = "Sair";
+  } else {
+    alert("Usuário ou senha inválidas");
+  }
+}
 
 
 
