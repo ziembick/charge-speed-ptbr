7f4270e0808791f0b37f4527b32d7b6ffc64b72b
Paulo Ziembick <pauloziembick@gmail.com>
2022-12-19 22:24:24 -0300

    adding page up css

--- js/script-company.js
+++ js/script-company.js
@@ -16,3 +16,56 @@ recrutar.addEventListener("click", function () {
   recrutarTexto.classList.remove('hide')
 
 });
+
+
+//btn top
+const btn = document.getElementById("btnTop");
+
+btn.addEventListener("click", function () {
+  window.scrollTo(0, 0);
+});
+
+document.addEventListener("scroll", ocultar);
+
+function ocultar() {
+  if (window.scrollY > 10) {
+    btn.style.display = "flex";
+  } else {
+    btn.style.display = "none";
+  }
+}
+
+ocultar();
+
+
+
+
+// function AddSelected () {
+//   if (classList === "tabMenu-recrutar__table") {
+//     style.display = "block"
+//   } else {
+//     style.display = "none"
+//   }
+// }
+
+// AddSelected ()
+
+
+
+// const btn = document.getElementById("btnTop");
+
+// btn.addEventListener("click", function () {
+//   window.scrollTo(0, 0);
+// });
+
+// document.addEventListener("scroll", ocultar);
+
+// function ocultar() {
+//   if (window.scrollY > 10) {
+//     btn.style.display = "flex";
+//   } else {
+//     btn.style.display = "none";
+//   }
+// }
+
+// ocultar();
