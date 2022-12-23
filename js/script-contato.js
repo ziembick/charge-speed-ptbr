d9d3091e7faeed95802c51b63a316f23de2628cd
Paulo Ziembick <pauloziembick@gmail.com>
2022-12-22 21:14:38 -0300

    page up top js

--- js/script-contato.js
+++ js/script-contato.js
@@ -22,3 +22,22 @@ function logar() {
     alert("Usuário ou senha inválidas");
   }
 }
+
+//btn page up
+const btn = document.getElementById("btnTop");
+
+btn.addEventListener("click", function () {
+  window.scrollTo(0, 0);
+});
+
+document.addEventListener("scroll", ocultar);
+
+function ocultar() {
+  if (window.scrollY > 10) {
+    btn.style.display = "flex";
+  } else {
+    btn.style.display = "none";
+  }
+}
+
+ocultar();
